5c8eb658ee2c7a87999e7f81793acb3094f14480
Martin Tichota <martin.tichota@gmail.com>
2016-06-12 22:13:53 +0200

    Set configuration, added committing and pushing commands.

--- clone.bat
+++ clone.bat
@@ -2,6 +2,7 @@
 set Path=%Path%;"c:\Program Files (x86)\Git\cmd\"
 git clone https://github.com/martintichota/git-push
 cd git-push
+git config --global push.default simple
 git fetch
 git checkout test
 pause
--- push.bat
+++ push.bat
@@ -1,5 +1,7 @@
 @echo off
 set Path=%Path%;"c:\Program Files (x86)\Git\cmd\"
 cd git-push
-git push
+git add -A 
+git commit
+git push origin
 pause
